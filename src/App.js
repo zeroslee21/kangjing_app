3f6658521d45b65a8ad49e7bf1c14bf26ee46f27
zeroslee21 <zeros.lee@gmail.com>
2020-11-10 16:35:51 +0900

    test3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
-import About2 from './routes/About2';
-import Navigation from './components/Navigation';
-import Detail from './routes/Detail';
-import Kang from './routes/Kang';
-import About from './routes/About'
+import React from "react";
+import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import About2 from "./routes/About2";
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
+import Kang from "./routes/Kang";
+import About from "./routes/About";
 
 function App() {
   return (
-    
     <HashRouter>
       <Navigation />
-    <Route path="/" exact={true} component={Kang}></Route>
-    <Route path="/About2" component={About2} />
-    <Route path="/About" component={About} />
-    <Route path="/detail" component={Detail} />
+      <Route path="/" exact={true} component={Kang}></Route>
+      <Route path="/About2" component={About2} />
+      <Route path="/About" component={About} />
+      <Route path="/detail" component={Detail} />
     </HashRouter>
-  )
+  );
 }
 
 export default App;
