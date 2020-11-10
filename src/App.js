6dd7c2bd868696881c3cc457d95c65f57d93f2aa
zeroslee21 <zeros.lee@gmail.com>
2020-11-10 17:12:41 +0900

    test8

--- src/App.js
+++ src/App.js
@@ -9,9 +9,13 @@ import About from "./routes/About";
 
 function App() {
   return (
-    <div className="test">
-      <h1> test</h1>
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Kang}></Route>
+      <Route path="/About2" component={About2} />
+      <Route path="/About" component={About} />
+      <Route path="/detail" component={Detail} />
+    </HashRouter>
   );
 }
 
