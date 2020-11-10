c7504d0fb0ef663ccca1815550acff52bc6c0901
zeroslee21 <zeros.lee@gmail.com>
2020-11-10 18:31:27 +0900

    test15

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import About2 from "./routes/About2";
 import Navigation from "./components/Navigation";
 import Detail from "./routes/Detail";
@@ -9,13 +9,13 @@ import About from "./routes/About";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Navigation />
       <Route path="/" exact={true} component={Kang}></Route>
       <Route path="/About2" component={About2} />
       <Route path="/About" component={About} />
       <Route path="/detail" component={Detail} />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
